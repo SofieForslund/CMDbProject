df3a57cca52da707dd08625bae6d5f0da67b5070
Sofie Forslund <sofo1400@student.miun.se>
2021-10-25 08:51:39 +0200

    Add project files.

---
+++ CMDBGrupp09/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMDBGrupp09.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
