d792c432ca108a6aecf150cc9ad15ebb8a291624
Sofie Forslund <sofo1400@student.miun.se>
2021-11-01 09:33:03 +0100

    skapat en js-fil för varje indexsida, arbetat med att ta fram/dölja fullständig plot. kvarstår att göra det för alla filmer på home index

---
+++ CMDBGrupp09/wwwroot/js/home.js
@@ -0,0 +1,26 @@
+﻿document.querySelector('#fullPlot').style.display = "none"
+
+document.querySelector('#expand').addEventListener("click", function () {
+
+    if (document.querySelector('#fullPlot').style.display === "none") {
+
+        document.querySelector('#fullPlot').style.display = "inline";
+        document.querySelector('#expand').innerHTML = 'Read less';
+        document.querySelector('#dots').style.display = 'none';
+        document.querySelector('#subString').style.display = 'none';
+    }
+    else {
+        document.querySelector('#fullPlot').style.display = "none";
+        document.querySelector('#expand').innerHTML = 'Read more';
+        document.querySelector('#dots').style.display = 'inline';
+        document.querySelector('#subString').style.display = 'inline';
+    }
+});
+
+/*
+
+let like = document.querySelector('#like')
+like.addEventListener('click', function (e) {
+
+})
+*/
