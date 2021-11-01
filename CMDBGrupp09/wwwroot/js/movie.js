b469166ea139bbe1330f9a785a5c6dc342a47864
Sofie Forslund <sofo1400@student.miun.se>
2021-11-01 17:25:11 +0100

    like och dislike funkar men behöver städas och lägga till att knapparna inte funkar medan like/dislike skickas

--- CMDBGrupp09/wwwroot/js/movie.js
+++ CMDBGrupp09/wwwroot/js/movie.js
@@ -1,30 +1,29 @@
-﻿//let allaLikes = 
+﻿document.querySelector('#ID').style.display = "none"
+
+
+
 
 document.querySelector('#like').addEventListener("click", async function () {
+    //let movieID = document.querySelector('#ID');
     let response = await fetch('https://grupp9.dsvkurs.miun.se/api/Movie/tt1853728/like')
-    if (response.status === 200) {
-        //ladda siffrorna på nytt
-        
-    }
-    else {
+    let movie = await response.json()
+    await update(movie)
 
-    }
+});
 
-    }
-)
+
 
 document.querySelector('#dislike').addEventListener("click", async function () {
     let response = await fetch('https://grupp9.dsvkurs.miun.se/api/Movie/tt1853728/dislike')
-    if (response.status === 200) {
-        //ladda siffrorna på nytt
-    }
-    else {
+    let movie = await response.json()
+    await update(movie)
 
-    }
+});
 
+function update(movie) {
+    document.querySelector('#likeNumber').innerHTML = movie.numberOfLikes;
+    document.querySelector('#dislikeNumber').innerHTML = movie.numberOfDislikes;
 }
-
-)
 
 //try {
 //    //ska bilden inte gå att klickas (preventdefault?)
