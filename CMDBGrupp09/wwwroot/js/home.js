c8ecd03a8fac855e7425f3aa9f31e865fb3d3063
Sofie Forslund <sofo1400@student.miun.se>
2021-11-04 16:21:55 +0100

    småjusteringar

--- CMDBGrupp09/wwwroot/js/home.js
+++ CMDBGrupp09/wwwroot/js/home.js
@@ -1,4 +1,6 @@
-﻿//från början är fullplot dold
+﻿
+
+//från början är fullplot dold
 let fullPlot = document.querySelector('#fullPlot')
 fullPlot.style.display = "none"
 let searchResult = document.querySelector('#result')
@@ -6,7 +8,6 @@ searchResult.style.display ="none"
 
 //variabler
 let subString = document.querySelector('#subString')
-let expand = document.querySelector('#expand')
 let button = document.querySelector('#searchButton')
 let searchBox = document.querySelector('#searchBox')
 let string
@@ -18,24 +19,19 @@ let counter
 //om ingen substring finns visas hela ploten
 if (subString.textContent === "N/A") {
     fullPlot.style.display = "inline";
-    expand.style.display = "none";
     subString.style.display = "none";
 }
 
 //klick på "read more"
-expand.addEventListener("click", function () {
+subString.addEventListener("click", function () {
+    fullPlot.style.display = "inline";
+    subString.style.display = "none";
+});
 
-    if (fullPlot.style.display === "none") {
-
-        fullPlot.style.display = "inline";
-        expand.innerHTML = 'Read less';
-        subString.style.display = "none";
-    }
-    else {
-        fullPlot.style.display = "none";
-        expand.innerHTML = 'Read more';
-        subString.style.display = "inline";
-    }
+//klick på "read less"
+fullPlot.addEventListener("click", function () {
+    fullPlot.style.display = "none";
+    subString.style.display = "inline";
 });
 
 //klick på sökknappen
@@ -46,8 +42,6 @@ button.addEventListener("click", function () {
     searchOmdb(searchString)
 });
 
-
-
 //fyller diven med sökresultat
 function updateSearchDiv(string) {
     movieHitsDiv = range.createContextualFragment(string)
@@ -57,40 +51,45 @@ function updateSearchDiv(string) {
 
 //skapar sökresultat
 async function searchOmdb(searchString) {
+
     try {
         let reponse = await fetch(`https://www.omdbapi.com/?apikey=750f36f6&s=${searchString}&plot=full`)
-        let omdbmovie = await reponse.json()
-        if (omdbmovie.Search.length < 5) {
-            counter = omdbmovie.Search.length
-        }
-        else {
-            counter = 5
-        }
+        let omdbmovieArray = await reponse.json()
+        counter = setCounter(omdbmovieArray)
+
         for (let i = 0; i < counter; i++) {
-            let imdbId = omdbmovie.Search[i].imdbID
+            let imdbId = omdbmovieArray.Search[i].imdbID
             string = `
-            <img src=${omdbmovie.Search[i].Poster} alt ="Film poster"/><br>
-            <h1 style="font-size: x-large"><a href="https://localhost:44319/Movie/Index/${imdbId}">${omdbmovie.Search[i].Title}</a></h1> 
-            <p>${omdbmovie.Search[i].Year}</p></br>`
+            <img src=${omdbmovieArray.Search[i].Poster} alt ="Film poster"/><br>
+            <h1 style="font-size: x-large"><a href="https://localhost:44319/Movie/Index/${imdbId}">${omdbmovieArray.Search[i].Title}</a></h1> 
+            <p>${omdbmovieArray.Search[i].Year}</p></br>`
             updateSearchDiv(string)
         };
-
+        //om användaren inte skrivit nåt i sökrutan
     } catch (e) {
         if (searchString === '') {
             string = `
             <h1>You didn't write a searchterm, try again!</h1>`
             updateSearchDiv(string)
 
-        }
+        } //om det inte blir några träffar
         else {
             string = `
             <h1>Your search for ${searchString} generated no hits!</h1>`
             updateSearchDiv(string)
         }
-    }
-      
+    } 
 }
 
+//om färre än 5 resultat, visa alla resultat
+function setCounter(omdbmovieArray) {
+    if (omdbmovieArray.Search.length < 5) {
+        return omdbmovieArray.Search.length
+    }
+    else {
+        return 5
+    }
+}
 
 
 
